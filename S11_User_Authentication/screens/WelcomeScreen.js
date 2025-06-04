f37993712726ae702deec8e5df09ddd64c057aca
Huy Hoang <hhoang12042003@gmail.com>
2025-06-04 17:56:35 +0700

    S11_User_Authentication

---
+++ S11_User_Authentication/screens/WelcomeScreen.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
+
+function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    async function getMessage() {
+      const response = await axios.get(
+        "https://react-native-course-bc612-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      );
+      setFetchedMessage(response.data);
+    }
+
+    getMessage();
+  }, []);
+
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+});
